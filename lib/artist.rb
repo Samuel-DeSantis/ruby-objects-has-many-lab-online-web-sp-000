9dd4baf3f4c374e6bdc6f96e763abfb4ca21d144
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-05 04:08:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,9 @@ class Artist
     song.artist = self #=> Passing in a Song object and adding the artist to that object
   end
 
-  def add_song_by_name(song)
-    song.artist.name = @name
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
 
   def song_count
